c5ba12c313a803259ee9020d47a0e93bc616497e
Dani-Bcn <nneodani@gmail.com>
2023-02-20 18:22:58 +0100

    Commit for deploy Netlify

--- src/components/PageInfoMovie.jsx
+++ src/components/PageInfoMovie.jsx
@@ -47,92 +47,101 @@ const PageInfoMovie = () => {
                 .then(resp => setInfoMovie(resp))
         }
     }, [count])
+    let img
+    if (infoMovie) {
+        img = `https://image.tmdb.org/t/p/w500/${infoMovie.poster_path}`
+    }
 
     return (
-        <main className='info-movie'>
-            {
-                infoMovie && (
-                    <section className='container-info-movie'>
-                        <article>
-                            <m.img src={`https://image.tmdb.org/t/p/w500/${infoMovie.poster_path}`} ></m.img>
-                        </article>
-                        <article className='card-info'>
-                            {
-                                infoMovie && (
-                                    <h2>{`${valueTitleOrname}`}</h2>
-                                )
-                            }
-                            {
-                                infoMovie.release_date && (
-                                    <h3>Release date <br /> <span> {infoMovie.release_date}</span></h3>
-                                )
-                            }
-                            <section>
-                                <h3>Cast</h3><br />
-                                {
-                                    namesfromCast && (
-                                        namesfromCast.cast.map((e, i) => (
-                                            i < 4 ? <span key={e.id}> {e.name}</span> : null
-                                        ))
-                                    )
-                                }
-                            </section>
-                            <h3>Language <br /> <span> {infoMovie.original_language}</span></h3>
-                            {
-                                infoMovie.homepage && (
-                                    <h3>Home page  <br /> <span> <a
-                                        style={{
-                                            fontSize: "1rem"
-                                        }}
-                                        href={infoMovie.homepage} target="_blank" rel="noopener noreferrer">{infoMovie.homepage}</a></span></h3>
-                                )
-                            }
-                            {
-                                infoMovie.runtime && (
-                                    <h3>Run time <br /> <span> {infoMovie.runtime}'</span></h3>
-                                )
-                            }
-                            {
-                                infoMovie.production_companies[0] && (
-                                    <h3>Productor company &nbsp; &nbsp;<span>{infoMovie.production_companies[0].name}</span></h3>
-                                )
-                            }
-                            {
-                                infoMovie.runtime && (
-                                    <h3>Vote average &nbsp; &nbsp;<span>{infoMovie.vote_average}</span></h3>
-                                )
-                            }
-                            {
-                                infoMovie && (
-                                    <div
-                                    >Genres  &nbsp; &nbsp;[
-                                        {
-                                            infoMovie.genres.map((element, index) => {
-                                                return (
-                                                    <span
-                                                        key={index}> &nbsp; {element.name}, &nbsp;</span>
-                                                )
-                                            })}
-                                        ]</div>
-                                )
-                            }
-                            <button onClick={() => navigate(-1)}>Back</button>
-                        </article>
-                    </section>
-                )
-            }
-            <article className='card-biography'>
+
+        <main className='back-img' style={{
+            backgroundImage: `url(${img})`
+        }}>
+            <div className='info-movie' >
                 {
                     infoMovie && (
-                        <h3>overview  <br />  <br />
-                            <span> {infoMovie.overview}</span>
-                        </h3>
+                        <section className='container-info-movie'>
+                            <article>
+                                <m.img src={`https://image.tmdb.org/t/p/w500/${infoMovie.poster_path}`} ></m.img>
+                            </article>
+                            <article className='card-info'>
+                                {
+                                    infoMovie && (
+                                        <h1>{`${valueTitleOrname}`}</h1>
+                                    )
+                                }
+                                {
+                                    infoMovie.release_date && (
+                                        <h3>Release date &nbsp; &nbsp; <span> {infoMovie.release_date}</span></h3>
+                                    )
+                                }
+                                <section>
+                                    <h3>Cast <br /> [ &nbsp;
+                                        {
+                                            namesfromCast && (
+                                                namesfromCast.cast.map((e, i) => (
+                                                    i < 4 ? <span key={e.id}> {e.name} | </span> : null
+                                                ))
+                                            )
+                                        }&nbsp; ]</h3>
+                                </section>
+                                <h3>Language &nbsp; &nbsp;<span> {infoMovie.original_language}</span></h3>
+                                {
+                                    infoMovie.homepage && (
+                                        <h3>Home page  &nbsp; &nbsp; <span> <a
+                                            style={{
+                                                fontSize: "1rem"
+                                            }}
+                                            href={infoMovie.homepage} target="_blank" rel="noopener noreferrer">{infoMovie.homepage}</a></span></h3>
+                                    )
+                                }
+                                {
+                                    infoMovie.runtime && (
+                                        <h3>Run time &nbsp; &nbsp; <span> {infoMovie.runtime}'</span></h3>
+                                    )
+                                }
+                                {
+                                    infoMovie.production_companies[0] && (
+                                        <h3>Productor company &nbsp; &nbsp;<span>{infoMovie.production_companies[0].name}</span></h3>
+                                    )
+                                }
+                                {
+                                    infoMovie.runtime && (
+                                        <h3>Vote average &nbsp; &nbsp;<span>{infoMovie.vote_average}</span></h3>
+                                    )
+                                }
+                                {
+                                    infoMovie && (
+                                        <div
+                                        >Genres  &nbsp; &nbsp;[
+                                            {
+                                                infoMovie.genres.map((element, index) => {
+                                                    return (
+                                                        <span
+                                                            key={index}> &nbsp; {element.name}  | </span>
+                                                    )
+                                                })}
+                                            ]</div>
+                                    )
+                                }
+                            </article>
+                        </section>
                     )
                 }
-            </article>
-            <>
-                <Cast valuesCast={infoMovie} funcCast={funcCast}></Cast>
-            </>
+                <article className='card-biography'>
+                    {
+                        infoMovie && (
+                            <h3>overview  <br />  <br />
+                                <span> {infoMovie.overview}</span>
+                            </h3>
+                        )
+                    }
+                </article>
+                <>
+                    <Cast valuesCast={infoMovie} funcCast={funcCast}></Cast>
+                </>
+                <button onClick={() => navigate(-1)}>Back</button>
+            </div>
         </main>
     );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,68 +1,85 @@
-import React, { Children, useEffect, useState } from 'react';
-import Infomovie from './ListMovies';
+import React, { useRef, useEffect, useState } from 'react';
 import ListMovie from './ListMovies'
-
 
 const Search = () => {
     const [infoMovie, setInfoMovie] = useState()
     const [movieOrTv, setMovieOrTv] = useState(["Movie", "Tv"])
     const [isSelected, setIsSelected] = useState("movie")
     const [localeMovieOrTv, setLocaleMovieOrTv] = useState(localStorage.getItem("movie-tv"))
-    const [changeCall, setChangeCall] = useState(false)
-    const [count, setCount] = useState(1)
+    const [changeCall, setChangeCall] = useState(true)
+    const [count, setCount] = useState()
     const [resApiGenres, setResApiGenres] = useState([])
-    const [genres, setGenres] = useState("")
+    const [genres, setGenres] = useState()
     const [searchPopularity, setSearchPopularity] = useState("")
     const valuesForPageInfo = [isSelected]
     let stringNum = toString()
-    localStorage.setItem("indexPage", count.toString())
+
+    localStorage.setItem("indexPage",1)
     stringNum = localStorage.getItem("indexPage")
+    const selectRef = useRef(null);
+    console.log(localStorage.getItem("indexPage"))
+    useEffect(() => {
+        if (!localStorage.getItem("indexPage")) {
+            setCount(1)
+        }
+    })
+
 
     const selected = ((target) => {
         if (target === "Movie") {
+            selectRef.current.selectedIndex = 0
             setCount(1)
             setIsSelected("movie")
             setChangeCall(!changeCall)
+            setGenres(28)
 
         } else {
+            selectRef.current.selectedIndex = 0
             setCount(1)
             setChangeCall(!changeCall)
             setIsSelected("tv")
+
+
+            setGenres(10759)
         }
     })
 
     const funcPopular = (() => {
         setSearchPopularity("&sort_by=vote_count.desc")
         setChangeCall(!changeCall)
+
     })
+
+
     const funcActual = (() => {
         setSearchPopularity("")
         setChangeCall(!changeCall)
     })
-
     const nextPage = (() => {
-        setCount(count + 1)
         console.log(count)
+        setCount(count + 1)
         setChangeCall(!changeCall)
     })
-
     const firtsPage = (() => {
-        setCount(1)
         console.log(count)
+        localStorage.setItem("indexPage", count)
+        setCount(1)
         setChangeCall(!changeCall)
     })
 
     const previousPage = (() => {
-        count < 2 ? null : setCount(count - 1)
         console.log(count)
+        localStorage.setItem("indexPage", count)
+        count < 2 ? null : setCount(count - 1)
         setChangeCall(!changeCall)
     })
 
     useEffect(() => {
-        fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=55b2cf9d90cb74c55683e395bb1ad12b&language=en-U")
+        fetch(`https://api.themoviedb.org/3/genre/${isSelected}/list?api_key=55b2cf9d90cb74c55683e395bb1ad12b&language=en-U`)
             .then(resp => resp.json())
             .then(resp => setResApiGenres(resp.genres))
     }, [changeCall])
+
     const selectGenre = ((target) => {
         setGenres(target.target.value)
         setChangeCall(!changeCall)
@@ -72,12 +89,13 @@ const Search = () => {
         fetch(`https://api.themoviedb.org/3/discover/${isSelected}?api_key=55b2cf9d90cb74c55683e395bb1ad12b${searchPopularity}&page=${stringNum}&with_genres=${genres}`)
             .then(resp => resp.json())
             .then(resp => setInfoMovie(resp.results))
+
     }, [changeCall])
 
     return (
         <main className='popular'>
             <section className='container-popular'>
-                <article className='container-button'>
+                <article className='container-nav'>
                     {
                         movieOrTv.map((e, i) => (
                             <h2 key={i} id={i} onClick={() => selected(e)}>{e}</h2>
@@ -85,9 +103,9 @@ const Search = () => {
                     }
                     <h2 onClick={() => funcActual()}>Current</h2>
                     <h2 onClick={() => funcPopular()}>Most popular</h2>
-                    <form >
-                        <h2>resApiGenres</h2>
-                        <select onChange={(e) => selectGenre(e)}>
+                    <form className='select-genres'>
+                        <h2>Genres</h2>
+                        <select ref={selectRef} onChange={(e) => selectGenre(e)}>
                             {
                                 resApiGenres.map((e, i) => (
                                     <option key={e.id} value={e.id} id={e.id}>{e.name}</option>
@@ -96,7 +114,7 @@ const Search = () => {
                         </select>
                     </form>
                 </article>
-                <nav className='container-button-popular'>
+                <nav className='container-nav'>
                     <button onClick={() => firtsPage()}>First</button>
                     <button onClick={() => previousPage()}>Previus</button>
                     <button onClick={() => nextPage()}>Next</button>
