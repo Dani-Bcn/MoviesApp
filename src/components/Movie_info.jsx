75a8f13222837c4918f6a16561bfb97a35b06fba
Dani-Bcn <nneodani@gmail.com>
2023-10-20 22:43:26 +0200

    Test page Movie_info

--- src/components/Movie_info.jsx
+++ src/components/Movie_info.jsx
@@ -4,10 +4,9 @@ import { motion as m } from "framer-motion";
 export default function Movie_info(props) {
   const { idMovie } = props;
   const [infoMovie, setInfoMovie] = useState();
-  const [infoCast, setInfoCast]=useState()
+  const [infoCast, setInfoCast] = useState();
 
   useEffect(() => {
-
     idMovie !== undefined
       ? fetch(
           `https://api.themoviedb.org/3/movie/${idMovie}?api_key=55b2cf9d90cb74c55683e395bb1ad12b`
@@ -15,16 +14,14 @@ export default function Movie_info(props) {
           .then((resp) => resp.json())
           .then((resp) => setInfoMovie(resp))
       : null;
- 
- 
-      fetch(
-        `https://api.themoviedb.org/3/movie/${idMovie}/credits?api_key=55b2cf9d90cb74c55683e395bb1ad12b`
-      )
-        .then((resp) => resp.json())
-        .then((resp) => setInfoCast((resp)))       
-   
- }, [idMovie]); 
-  idMovie ?console.log(infoMovie):null
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/${idMovie}/credits?api_key=55b2cf9d90cb74c55683e395bb1ad12b`
+    )
+      .then((resp) => resp.json())
+      .then((resp) => setInfoCast(resp));
+  }, [idMovie]);
+  idMovie ? console.log(infoMovie) : null;
   return (
     <m.main
       className="
@@ -34,9 +31,9 @@ export default function Movie_info(props) {
         bg-slate-800
         z-20
     "
-    animate={{
-        opacity:[0,1]
-    }}
+      animate={{
+        opacity: [0, 1],
+      }}
     >
       {infoMovie ? (
         <>
@@ -49,8 +46,8 @@ export default function Movie_info(props) {
                 h-[500px]                 
             "
             src={`https://image.tmdb.org/t/p/w500/${infoMovie.poster_path}`}
-          />        
-        <section
+          />
+          <section
             className="
                 w-screen
                 flex
@@ -60,26 +57,52 @@ export default function Movie_info(props) {
                 gap-5
                 p-10
                 "
-        >
+          >
             <h2
-                className="
+              className="
                 text-2xl
                 "
-            >{infoMovie.title}</h2>
+            >
+              {infoMovie.title}
+            </h2>
             <p>{infoMovie.overview}</p>
             <section
-                className="
+              className="
                 w-screen
                 flex
                 flex-col
                 items-start
                 px-10
+                gap-5
                 "
             >
-              <p> Runtime {infoMovie.runtime }"</p>  
+              <section
+                className="
+                    flex
+                    w-full
+                    justify-between
+                "
+              >
+                <p>{infoMovie.release_date.slice(0, 4)}</p>
+                <p>{infoMovie.runtime}"</p>
+                <p>{infoMovie.original_language}</p>
+
+                <p>{infoMovie.production_companies[0].origin_country}</p>
+              </section>
+              <section
+                className="
+                    flex
+                    w-full
+                    flex-wrap
+                    gap-5
+                "
+              >
+                {infoMovie.genres.map((e) => {
+                  return <p> {e.name}</p>;
+                })}
+              </section>
             </section>
-            
-        </section>
+          </section>
         </>
       ) : null}
     </m.main>
