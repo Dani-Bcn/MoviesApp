110388ec8ea5a793954ac35cdda62e8df5bc871c
Dani-Bcn <nneodani@gmail.com>
2023-10-23 04:30:57 +0200

    Test video trailer responsive

--- src/components/Get_videos.jsx
+++ src/components/Get_videos.jsx
@@ -15,85 +15,97 @@ export default function Get_videos(props) {
   const [getKeyMovie, setGetKeyMovie] = useState();
 
   useEffect(() => {
+
     selectMovieTv ? setSelectMovieOrTv("movie") : setSelectMovieOrTv("tv");
     console.log(selectMovieOrTv, idMovie);
-
     selectMovieOrTv && idMovie
+
       ? fetch(
           `https://api.themoviedb.org/3/${selectMovieOrTv}/${idMovie}/videos?api_key=55b2cf9d90cb74c55683e395bb1ad12b`
         )
           .then((resp) => resp.json())
           .then((resp) => setResApi(resp))
       : null;
+
   }, [selectMovieOrTv]);
+
   useEffect(() => {
     resApi ? setMaxResultsTitle(resApi.results.slice(0, 10)) : null;
     resApi ? console.log(resApi) : null;
   }, [resApi]);
 
   return (
-    <main className="w-screen h-screen bg-slate-600 z-50">
-      <h1 className="text-10xl text-red-800" onClick={() => setActive(!active)}>
-        videos
-      </h1>
-      {resApi ? (
-        resApi.results.length === 0 ? (
-          <h2
-            className=" 
-                w-screen
-                flex
-                justify-center
-                text-orange-200
-                text-5xl
-                m-auto
-            "
-          >
-            Sorry No trailer
-          </h2>
-        ) : maxResultsTitle ? (
+    <main
+        className="
+            absolute
+            w-screen
+            h-screen
+            z-20
+            bg-slate-800
+        "
+    >
+      <section
+        className="
+           w-full
+           h-full
+           flex
+        "
+      >
+        {resApi ? (
+          resApi.results.length === 0 ? (
+            <h2>
+              No trailer
+            </h2>
+          ) : maxResultsTitle ? (
             <section>
-
-                {
-          maxResultsTitle.map((e, i) => {
-            return (
-              <section
-              key={i}
-              >
-                <section                 
-                  className="
-                    mx-5                        
-                    text-orange-200
-                    text-[1.2rem]
-                "
-                >
-                  <button
-                    onClick={() => {
-                      console.log(e), setGetKeyMovie(e.key);
-                    }}
+                <section
                     className="
-                        flex
-                        justify-start  
-                        w-60
-                        py-5
+                    w-screen
+                    p-5
                     "
-                  >
-                    {e.name.slice(0, 24)}
-                  </button>
-                </section>               
+                >
+              {maxResultsTitle.map((e, i) => {
+                return (
+                 
+                      <button key={i}
+                      className="
+                        w-80
+                        flex
+                        items-start
+                        justify-start
+                        text-[0.75rem]
+                        py-2
+                      "
+                        onClick={() => {
+                          console.log(e), setGetKeyMovie(e.key);
+                        }}>
+                        {e.name}
+                      </button>   
+                 
+                );
+              })}
+            </section>
+              <iframe
+               className="
+               w-screen
+               h-56
+               sm:h-96
+                my-10
+               " 
+               src={`//www.youtube.com/embed/${getKeyMovie}?autoplay=1;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=es&amp;modestbranding=1&amp;fs=1&amp;autohide=1`}
+              ></iframe>
               </section>
-            );
-          })
-        }
-          <iframe
-          src={`//www.youtube.com/embed/${getKeyMovie}?autoplay=1;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=es&amp;modestbranding=1&amp;fs=1&amp;autohide=1`}
-        ></iframe>
-        </section>          
-        ) : (
-          <iframe
-            src={`//www.youtube.com/embed/${resApi.results[0].key}?autoplay=1;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=es&amp;modestbranding=1&amp;fs=1&amp;autohide=1`}
-          ></iframe>
-        )
-      ) : null}
+         
+          ) : (
+            <iframe
+              src={`//www.youtube.com/embed/${resApi.results[0].key}?autoplay=1;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=es&amp;modestbranding=1&amp;fs=1&amp;autohide=1`}
+            ></iframe>
+          )
+   
+        ) : null}
+
+
+      </section>
     </main>
   );
 }
