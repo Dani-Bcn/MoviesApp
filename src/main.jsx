13bedfa71ab0f87805e4c70bbcab8d2d1a197a41
Dani-Bcn <nneodani@gmail.com>
2023-10-23 18:21:59 +0200

    Test Hasshouter to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { AnimatePresence } from "framer-motion";
-import { BrowserRouter, useLocation } from "react-router-dom";
+import { HashRouter, useLocation } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
   <AnimatePresence>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AnimatePresence>
   
 );
