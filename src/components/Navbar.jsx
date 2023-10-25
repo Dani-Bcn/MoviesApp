77152ba995ace25b1544218023a90253ff2f3d02
Dani-Bcn <nneodani@gmail.com>
2023-10-25 04:32:11 +0200

    Test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,18 +12,14 @@ export default function Navbar(props) {
   return (
     <main>
       <section
-        className="
-          flex
-          items-center
-          justify-start
-        "
+        className="flex items-center justify-start "
       >
         {location.pathname !== "/" ? (
           <button
             className="
               absolute
               w-5              
-              px-10
+              px-5
             "
             onClick={() => {
               navigate(-1), console.log(location.pathname);
@@ -36,45 +32,24 @@ export default function Navbar(props) {
         <h1
           onClick={() => navigate("/")}
           className="
-              w-screen
-              my-5
-              flex
-              items-center
-              justify-center
-              text-2xl     
-              text-orange-200
-            "
+              w-screen my-5 flex items-center justify-center text-2xl text-orange-200 "
         >
           App Movies
         </h1>
       </section>
       <section
-        className="
-            w-full
-            my-5
-            flex
-            items-center
-            justify-center
-            z-50
-        "
+        className="m-5 flex items-center justify-around z-50"
       >        
         <button
           onClick={() => {
             selectMovieOrTv(true), navigate("/");
-          }}
-          className="
-            w-12
-          "
-        >
+          }}>
           Movies
         </button>
         <button
           onClick={() => {
             selectMovieOrTv(false), navigate("/");
           }}
-          className="
-         w-40
-       "
         >
           Tv
         </button>
@@ -84,12 +59,7 @@ export default function Navbar(props) {
           height="25px"
           viewBox="0 0 24 24"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="
-            -pl-2
-            ml-5
-          "
-        >
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10 4a6 6 0 1 0 0 12 6 6 0 0 0 0-12zm-8 6a8 8 0 1 1 14.32 4.906l5.387 5.387a1 1 0 0 1-1.414 1.414l-5.387-5.387A8 8 0 0 1 2 10z"
             fill="#599"
