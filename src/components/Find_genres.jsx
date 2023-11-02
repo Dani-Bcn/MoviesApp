1d44954233ce54d705f45785fe5e276ce404969b
Dani-Bcn <nneodani@gmail.com>
2023-11-02 11:18:59 +0100

    test

--- src/components/Find_genres.jsx
+++ src/components/Find_genres.jsx
@@ -4,22 +4,29 @@ export default function Find_genres() {
   const [dataGenres, setDataGenres] = useState();
   const [dataMovies, setDataMovies] = useState([]);
   const movieOrTv = localStorage.getItem("movieOrTv");
+  const [active,setActive] = useState(false)
 
+useEffect(()=>{
+
+  movieOrTv==="movie" ?setActive(true):setActive(false)
+  dataMovies.splice(0,dataMovies.length)
+console.log(active)
+},[movieOrTv])
+
+
+ 
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/genre/${movieOrTv}/list?api_key=55b2cf9d90cb74c55683e395bb1ad12b`
     )
       .then((resp) => resp.json())
-      .then((resp) => setDataGenres(resp));
-  }, []);
+      .then((resp) => setDataGenres(resp)); 
+  }, [movieOrTv]);
 
-  dataGenres ? console.log("genres", dataGenres.genres) : null;
+  dataGenres ? console.log("genres", dataGenres.genres) : null; 
   useEffect(() => {
     if (dataGenres) {
-      (movieOrTv === "movie" && dataGenres.length > 18) ||
-      (movieOrTv === "tv" && dataGenres.length > 16)
-        ? dataMovies.splice(0, dataMovies.length) 
-        : null;
+      
       dataGenres.genres.map((e, i) => {  
         fetch(
           `https://api.themoviedb.org/3/discover/${movieOrTv}?api_key=55b2cf9d90cb74c55683e395bb1ad12b&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${e.id}`
@@ -31,38 +38,37 @@ export default function Find_genres() {
       });
     }
   }, [dataGenres]);
-  dataMovies ? console.log(dataMovies) : null;
+  dataMovies ? console.log(dataMovies) : null; 
 
   return (
     <main>
-      {dataMovies
-        ? (movieOrTv === "movie") & (dataMovies.length === 19) ||
-          (movieOrTv === "tv") & (dataMovies.length === 16)
-          ? dataMovies.map((e, i) => {
-              console.log(dataMovies);
+      <section className="mt-24">
+      {dataMovies?
+        dataMovies.map((e, i) => {
+
               return (
+                <section key={i}>
+                       <p className="text-orange-100 text-2xl " >{dataGenres.genres[i].name}</p>
                 <section
-                  key={i}
-                  className="flex h-[275px] w-screen overflow-y-hidden scroll-auto"
+                  className="flex h-[250px] mx-5 gap-5 w-screen overflow-y-hidden scroll-auto"
                 >
-                  {/* <p className="fixed text-red-300">
-                    {dataGenres.genres[i].name}
-                  </p> */}
+              
 
                   {e.map((e, i) => (
                     <>
                       <img
                         key={i}
-                        className="m-5rounded-[10px]"
+                        className="rounded-[10px]"
                         src={`https://image.tmdb.org/t/p/w500/${e.poster_path}`}
                       />
                     </>
                   ))}
                 </section>
+                </section>
               );
             })
-          : null
         : null}
+        </section>
     </main>
   );
 }
