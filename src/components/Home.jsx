86cbee3cbf5be7b9263ef1e30200fea67c7970a8
Dani-Bcn <nneodani@gmail.com>
2023-11-23 07:44:11 +0100

    test responsive title done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ import {motion as m} from "framer-motion"
 
 export default function Home() {
  return (
-  <main className="flex flex-col items-center overflow-hidden ">
+  <main className="flex flex-col items-center  ">
      <section
-        className=" absolute z-20 -mt-[200px]  lg:flex lg:mt-32 lg:scale-150 flex flex-col items-center justify-center"      
+        className=" absolute z-20 mt-32 md:hidden lg:flex lg:scale-150 flex flex-col items-center justify-center"      
       >
         <h1
           className=" text-blue-200 text-8xl font-Josefin  text-shadow-logo"         
