91ba30ce6998d59af2a65c10dfb5f88cc50788e0
Dani-Bcn <nneodani@gmail.com>
2023-11-25 12:01:53 +0100

    test

--- src/components/Trailers.jsx
+++ src/components/Trailers.jsx
@@ -1,43 +1,51 @@
 import React, { useState, useEffect } from "react";
-import {motion as m} from "framer-motion"
+import { motion as m } from "framer-motion";
 import ReactPlayer from "react-player";
+import { useNavigate } from "react-router-dom";
 
 export default function Trailers() {
   const movieOrTv = localStorage.getItem("movieOrTv");
   const [idMovie, setIdMovie] = useState(localStorage.getItem("idMovie"));
+  const navigate = useNavigate()
 
   const [dataVideos, setDataVideos] = useState();
-
+ window.addEventListener("resize", ()=> navigate("/infoMovie"))
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/${movieOrTv}/${idMovie}/videos?api_key=55b2cf9d90cb74c55683e395bb1ad12b`
     )
       .then((resp) => resp.json())
       .then((resp) => setDataVideos(resp.results));
+
+      
   }, [idMovie]);
 
-
+  
+   
+  
 
   return (
     <main className="my-20">
       {dataVideos
         ? dataVideos.map((e, i) => (
-            <m.section key={i} className="opacity-0 z-10 my-10 md:mb-80"
-              whileInView={{               
-                opacity:1
+            <m.section
+              key={i}
+              className="opacity-0 z-10 my-10 md:mb-80"
+              whileInView={{
+                opacity: 1,
               }}
             >
-                 <ReactPlayer
-               id="md-movie"
-                className="border-2 m-auto border-orange-300 z-40 shadow-xl shadow-slate-950"
-                url={`//www.youtube.com/watch?v=${e.key}`}
-                        
-             playing={false}
-             controls={true}
-             width={380}
-             height={250}
-               
-              ></ReactPlayer>
+              {i< 10 ? (
+                <ReactPlayer
+                  id="md-movie"
+                  className="border-2 m-auto border-orange-300 z-40 shadow-xl shadow-slate-950"
+                  url={`//www.youtube.com/watch?v=${e.key}`}
+                  playing={false}
+                  controls={true}
+                  width={380}
+                  height={250}
+                ></ReactPlayer>
+              ) : null}
             </m.section>
           ))
         : null}
